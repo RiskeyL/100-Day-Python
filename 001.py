d407e7fce2f0015c2c74232b006332502649ccf8
Riskey <7a8y@163.com>
2025-10-27 22:34:00 +0800

    Day 1 course completed

--- 001.py
+++ 001.py
@@ -1,1 +1,7 @@
-print("Hell World")
+print("Welcom to the Band Name Generator :P")
+
+city_name = input("What's the name of the city you grew up in?\n")
+
+pet_name = input("What's your pet's name?\n")
+
+print("Your band name should be " + city_name + " " + pet_name)
