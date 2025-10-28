02f5f5441cc4d82b45e47533506a080af0c43089
Riskey <7a8y@163.com>
2025-10-29 00:06:42 +0800

    Day 2 - wip

---
+++ 002.py
@@ -0,0 +1,19 @@
+# print("hELLO"[2])
+
+# print(122 + 456)
+
+# print("122" + "122")
+
+# print(12_345_453)
+
+# print(12,345,453)
+
+# Boolean values are case-sensitive
+# print(False)
+
+
+# print(len("123454"))
+
+# print(type(123.456))
+
+print("Number of letters in your name: " + str(len(input("Enter your name\n"))))
