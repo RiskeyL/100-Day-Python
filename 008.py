2a3637c20e5e74dc64bac2e2419d5fa1ee45ac74
Riskey <riskey47@dify.ai>
2025-11-17 13:32:55 +0800

    Day 8 mid-course

---
+++ 008.py
@@ -0,0 +1,12 @@
+# def great():
+#     print("This is a great function!")
+#     print("It does wonderful things.")
+#     print("You should definitely use it!")
+
+
+def great(name):
+    print(f"This is a great function, {name}!")
+    print("It does wonderful things.")
+    print("You should definitely use it!")
+
+great("Alice")
